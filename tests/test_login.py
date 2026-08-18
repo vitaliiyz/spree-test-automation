e699ffcccc96a9784d70c22693ab1854feb5dda8
vitaliiyz <vz260198@gmail.com>
2026-08-18 20:04:30 +0200

    Update README.md with project details and enhance test_login.py for consistency

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,11 +2,11 @@ from playwright.sync_api import Page, expect
 
 
 def test_main_page(page: Page):
-    page.goto('http://localhost:3001')
-    expect(page.get_by_text('Spree Store Storefront')).to_be_visible()
+    page.goto("http://localhost:3001")
+    expect(page.get_by_text("Spree Store Storefront")).to_be_visible()
 
 
 def test_login(page: Page):
-    page.goto('http://localhost:3001')
+    page.goto("http://localhost:3001")
     page.locator('//a[@aria-label="Account"]').click()
-    expect(page.locator('[data-slot="card-title"]'), 'No element found').to_have_text('My Account')
+    expect(page.locator('[data-slot="card-title"]'), "No element found").to_have_text("My Account")
