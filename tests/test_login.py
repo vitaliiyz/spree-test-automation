6e7b84da5e6845ddd245745cbdde2e2329bf3611
vitaliiyz <vz260198@gmail.com>
2026-08-18 18:21:40 +0200

    Update dependencies in pyproject.toml and enhance test_login.py with new tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,2 +1,12 @@
-def test():
-    assert 1 == 1
+from playwright.sync_api import Page, expect
+
+
+def test_main_page(page: Page):
+    page.goto('http://localhost:3001')
+    expect(page.get_by_text('Spree Store Storefront')).to_be_visible()
+
+
+def test_login(page: Page):
+    page.goto('http://localhost:3001')
+    page.locator('//a[@aria-label="Account"]').click()
+    expect(page.locator('[data-slot="card-title"]'), 'No element found').to_have_text('My Account')
